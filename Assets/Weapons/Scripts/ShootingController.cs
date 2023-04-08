1cb54a8ef1fcbf41d926d7c0097d774daec22166
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2023-04-08 15:32:18 +0200

    fixed the shooting script
    Now shoot only one bullet at mouse position and gun rotation

--- Assets/Weapons/Scripts/ShootingController.cs
+++ Assets/Weapons/Scripts/ShootingController.cs
@@ -8,12 +8,10 @@ using UnityEngine.Serialization;
 
 public class ShootingController : MonoBehaviour
 {
-    [SerializeField] private Transform weapon;
-    [SerializeField] private float bulletSpeed = 10.0f;
-    [SerializeField] private float damage = 10.0f;
+    [SerializeField] private Transform firePoint;
     [SerializeField] private float fireRate = 5.0f;
-    [SerializeField] private float range = 50.0f;
-    [SerializeField] private Rigidbody projectile;
+    [SerializeField] private GameObject projectile;
+
 
     private PlayerInputAction _playerInput;
     private Camera _mainCamera;
@@ -33,35 +31,22 @@ public class ShootingController : MonoBehaviour
         _mainCamera = Camera.main;
     }
 
-    private void Update()
-    {
-        if (_isFiring)
-        {
-            FireWeapon();
-        }
-    }
-
     private void OnFire(InputAction.CallbackContext context)
     {
         var click = context.ReadValue<float>();
         print(click);
         if (context.performed)
         {
-            if (_isFiring == true) return;
-            _isFiring = true;
-        }
-        else if (context.canceled)
-        {
-            if (_isFiring == false) return;
-            _isFiring = false;
+            FireWeapon();
         }
     }
 
     private void FireWeapon()
     {
-        var bullet = Instantiate(projectile, weapon.position,
-            Quaternion.identity);
-        bullet.AddForce(bullet.transform.forward * fireRate);
+        var bullet = Instantiate(projectile, firePoint.position,
+            firePoint.rotation);
+        var rigidbody = bullet.GetComponent<Rigidbody>();
+        rigidbody.AddForce(firePoint.right * fireRate, ForceMode.Impulse);
     }
 
     private void OnEnable()
