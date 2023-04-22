692413d31565730b9d63671e2f545cbd3403fc2e
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2023-04-22 11:28:55 +0200

    Split the animation controller into moving and fire controller to reuse on other game objects

---
+++ Assets/Characters/Shared/Scripts/MovingAnimationController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Characters.Shared.Scripts
+{
+    public class MovingAnimationController : MonoBehaviour
+    {
+        private Animator _animator;
+        private CharacterController _characterController;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _characterController = GetComponent<CharacterController>();
+        }
+        
+        private void Update()
+        {
+            MovingAnimation();
+        }
+
+        private void MovingAnimation()
+        {
+            // Retrieve the speed attribute from the CharacterController
+            var speed = _characterController.velocity
+                .magnitude;
+            _animator.SetFloat("MovingSpeed", speed);
+        }
+    }
+}
---
+++ Assets/Characters/Shared/Scripts/ShootingAnimationController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Characters.Shared.Scripts
+{
+    public class ShootingAnimationController : MonoBehaviour
+    {
+        private PlayerInput _playerInput;
+        private Animator _animator;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _playerInput = GetComponent<PlayerInput>();
+            
+            // Bind shooting animation the the tate of the Fire event
+            _playerInput.actions["Fire"].performed += Fire;
+            _playerInput.actions["Fire"].canceled += StopFire;
+        }
+
+        private void Fire(InputAction.CallbackContext context)
+        {
+            _animator.SetBool("IsFiring", true);
+        }
+
+        private void StopFire(InputAction.CallbackContext context)
+        {
+            _animator.SetBool("IsFiring", false);
+        }
+    }
+}
