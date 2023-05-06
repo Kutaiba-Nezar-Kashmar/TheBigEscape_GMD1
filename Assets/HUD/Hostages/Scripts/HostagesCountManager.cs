2115490798132ba7ef247794c068d231123efc28
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2023-05-06 23:28:19 +0200

    Now the game displayes hou many npcs are there as part of the optional task

---
+++ Assets/HUD/Hostages/Scripts/HostagesCountManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace HUD.Hostages.Scripts
+{
+    public class HostagesCountManager : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI hostagesText;
+        private void Update()
+        {
+            var myPrefabObjects = GameObject.FindGameObjectsWithTag("NPC");
+            var numberOfMyPrefab = myPrefabObjects.Length;
+            hostagesText.text = $"Hostages: {numberOfMyPrefab}";
+        }
+    }
+}
