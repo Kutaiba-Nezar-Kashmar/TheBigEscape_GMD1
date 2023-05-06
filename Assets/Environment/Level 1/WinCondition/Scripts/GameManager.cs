3cc37246e0d019299424fd051ca07f01a03a5e71
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2023-05-06 23:10:30 +0200

    Implemented a win window

---
+++ Assets/Environment/Level 1/WinCondition/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Environment.Level_1.WinCondition.Scripts
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] private LayerMask escapePoint;
+        [SerializeField] private float winAriaRange = 1.0f;
+        [SerializeField] private GameObject winText;
+
+        private void Update()
+        {
+            if (Physics.CheckSphere(transform.position,
+                    winAriaRange,
+                    escapePoint))
+            {
+                Time.timeScale = 0;
+                winText.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Environment/Level 1/WinCondition/Scripts/WinMenuManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Environment.Level_1.WinCondition.Scripts
+{
+    public class WinMenuManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject menu;
+        
+        private void Awake()
+        {
+            menu.SetActive(false);
+        }
+
+        public void NextLevel()
+        {
+           Debug.Log("No extra levels...for now");
+        }
+
+        public void ToMenu()
+        {
+            menu.SetActive(false);
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
